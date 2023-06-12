af132bc880b200e0c8f062f513b1ee1622da2484
abhinavfury13 <abhinavramnath13@gmail.com>
2023-06-12 20:50:25 +0530

    Update multiboot-header.asm

--- src/arch/x86_64/multiboot-header.asm
+++ src/arch/x86_64/multiboot-header.asm
@@ -6,7 +6,7 @@ header_start:
     dd header_end - header_start ; header length
     ; checksum
     dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
-
+    
     ; required end tag
     ; dw is define word (16-bit)
     dw 0    ; type
