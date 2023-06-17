49105e39ac608b674513d89b75b89a7760e40242
abhinavfury13 <abhinavramnath13@gmail.com>
2023-06-17 13:10:43 +0530

    clear all the data segment registers

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -4,6 +4,14 @@ section .text
 bits 64
 
 long_mode_start:
+    ; clear all the data segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
     ; 48 65 6C 6C 6F 20 57 6F 72 6C 64 (hello world in hex)
     ;print Hello World to Screen
     mov rax, 0x2f6c2f6c2f652f48
